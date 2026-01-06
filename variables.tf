b661692a6599dc3dc80d1752c855973661c66e5c
Faizan kaishar <115065680+faiz1487@users.noreply.github.com>
2026-01-06 15:03:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,6 +6,7 @@ variable "instance_type" {
   default = "t3.micro"
 }
 
-variable "AKIAUY5PSOQB4WOQYJYI" {
-  description = "r1Mca0ZT9DvLHRvK7rgRWZVjuhXYUSzLCNNN2ThT"
+variable "Access_key" {
+  description = "secret_key"
 }
+
